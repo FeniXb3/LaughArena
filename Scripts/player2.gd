29e009439929571134959c3803d7652c39a855bc
Krzysztof Niedzielczyk <khristofor94@gmail.com>
2024-01-27 13:44:50 +0100

    movement

--- Scripts/player2.gd
+++ Scripts/player2.gd
@@ -1,11 +1,14 @@
 extends RigidBody2D
 
 @export var speed: float = 1
+var input_direction = Vector2.ZERO
 
 func get_input():
-	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	linear_velocity = input_direction * speed
-	return linear_velocity
+	input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+
+
+func _process(delta):
+	get_input()
 
 func _physics_process(delta):
-	move_and_collide(get_input())
+	move_and_collide(input_direction * speed)

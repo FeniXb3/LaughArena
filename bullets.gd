13296077a7e87d205f862da70a7d5d3cb2e0aeb3
Mateusz0lszewski <mateuszolszewski@opoczta.pl>
2024-01-27 12:38:06 +0100

    celujący turret+czyszczenie

--- bullets.gd
+++ bullets.gd
@@ -1,18 +1,18 @@
 extends RigidBody2D
 
 var direction: Vector2 = Vector2.RIGHT
-var speed: float = 300
+#var speed: float = 300
 
-const RIGHT = Vector2.RIGHT
-@export var SPEED: int = 200
+#const RIGHT = Vector2.RIGHT
+#@export var SPEED: int = 200
 
-func _ready():
-	var movement = RIGHT.rotated(rotation) * SPEED
+#func _ready():
+	#var movement = RIGHT.rotated(rotation) * SPEED
 	
-	apply_impulse(movement)
+	#apply_impulse(movement)
 
-func _physics_process(delta):
-	var movement = direction * SPEED * delta
+#func _physics_process(delta):
+	#var movement = direction * SPEED * delta
 
 #func _physics_process(delta):
 	#position += direction * speed * delta
@@ -23,3 +23,5 @@ func _on_screen_exited():
 
 func _on_body_entered(body):
 	queue_free()
+
+

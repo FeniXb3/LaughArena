b84f9cde2c2d86da8562f2b6365b6f7cc4432f0a
Krzysztof Niedzielczyk <khristofor94@gmail.com>
2024-01-27 22:35:43 +0100

    dodane odbicie

--- Scripts/player2.gd
+++ Scripts/player2.gd
@@ -6,24 +6,34 @@ extends RigidBody2D
 @export var max_mass = 1000
 @export var min_mass = 0.01
 var input_direction = Vector2.ZERO
+@onready var area_2d = %Area2D
+@export var push_force = 1000
 
 func _ready():
 	SignalBus.health_decreased.connect(_on_health_decreased)
 	SignalBus.health_increased.connect(_on_health_increased)
-	
+
+
 func get_input():
 	input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+
 
 func _on_health_decreased(value):
 	mass = clamp(mass - value, min_mass, max_mass)
 	if mass <= 0.01:
 		SignalBus.game_over.emit()
-			
+
+
 func _on_health_increased(regen):
 	mass = clamp(mass + regen, min_mass, max_mass)
 
+
 func _process(_delta):
 	get_input()
+
+
+	if Input.is_action_just_pressed("Shoot"):
+		_deflection()
 
 func _physics_process(_delta):
 	move_and_collide(input_direction * speed)
@@ -31,3 +41,12 @@ func _physics_process(_delta):
 
 func _on_timer_timeout():
 	SignalBus.health_increased.emit(health_regeneration)
+
+
+func _deflection():
+	var bulletsArray = area_2d.get_overlapping_bodies()
+	for bullet in bulletsArray:
+		var direction = (bullet.transform.origin - transform.origin)
+		
+		bullet.linear_velocity = Vector2.ZERO
+		bullet.apply_impulse(direction * push_force)
